79e9f45c454856f60716f778ba7263ca62392caa
Mohammed Aslaan <81412984+Mohammedaslaan@users.noreply.github.com>
2023-12-23 17:25:13 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,7 @@
-
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-files-with-jenkins"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"  # Replace with your desired AWS region
+  }
+}
